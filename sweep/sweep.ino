890c9ee68d20829b552bb46394154923a1f78f49
Daniel Barszczak <daniel@Izas-Mac-297.local>
2019-07-02 15:04:39 -0700

    Update sweep.ino

--- sweep/sweep.ino
+++ sweep/sweep.ino
@@ -4,9 +4,9 @@ int second = 10;
 int third = 9;
 int fourth = 6;
 int pos1 = 180; //position for each servo
-int pos2 = 0;
-int pos3 = 0;
-int pos4 = 0;
+int pos2 = 90;
+int pos3 = 90;
+int pos4 = 90;
 Servo clawServo; //make each servo
 Servo servo2;
 Servo servo3;
@@ -29,48 +29,71 @@ void loop() {
   servo2.write(pos2);
   servo3.write(pos3);
   servo4.write(pos4);
-  
+
   if (Serial.available()) { //if there is something inputted into the serial
     char command = Serial.read(); //what is being read
-    if(command == '1' || command == '2' || command == '3' || command == '4'){ //if it is a number
+    if (command == '1' || command == '2' || command == '3' || command == '4') { //if it is a number
       Serial.print("You are currently using servo number ");
       Serial.println(command);
       servo = command; //set the servo to be edited to the servo
-    } else if(command == '<'){ //decrement command
-      if(servo == '1'){ //for the specified servo
-        if(pos1 - change >= 90){ //if the change will go out of bounds
+    } else if (command == '<') { //decrement command
+      if (servo == '1') { //for the specified servo
+        if (pos1 - change >= 90) { //if the change will go out of bounds
           pos1 = pos1 - change; //change position
+          Serial.println(pos1);
+        } else {
+          Serial.println("This servo can't move past this angle");
         }
-      } else if(servo == '2'){
-        if(pos2 - change >= 0){
+      } else if (servo == '2') {
+        if (pos2 - change >= 0) {
           pos2 = pos2 - change;
           Serial.println(pos2);
+        } else {
+          Serial.println("This servo can't move past this angle");
         }
-      } else if(servo == '3'){
-        if(pos3 - change >= 0){
+      } else if (servo == '3') {
+        if (pos3 - change >= 0) {
           pos3 = pos3 - change;
+          Serial.println(pos3);
+        } else {
+          Serial.println("This servo can't move past this angle");
         }
-      } else if(servo == '4'){
-        if(pos4 - change >= 0){
+      } else if (servo == '4') {
+        if (pos4 - change >= 0) {
           pos4 = pos4 - change;
+          Serial.println(pos4);
+        } else {
+          Serial.println("This servo can't move past this angle");
         }
       }
-    } else if(command == '>'){ //increment command
-      if(servo == '1'){
-        if(pos1 + change <= 180){
+    } else if (command == '>') { //increment command
+      if (servo == '1') {
+        if (pos1 + change <= 180) {
           pos1 = pos1 + change;
+          Serial.println(pos1);
+        } else {
+          Serial.println("This servo can't move past this angle");
         }
-      } else if(servo == '2'){
-        if(pos2 + change <= 180){
+      } else if (servo == '2') {
+        if (pos2 + change <= 180) {
           pos2 = pos2 + change;
+          Serial.println(pos2);
+        } else {
+          Serial.println("This servo can't move past this angle");
         }
-      } else if(servo == '3'){
-        if(pos2 + change <= 180){
+      } else if (servo == '3') {
+        if (pos3 + change <= 180) {
           pos3 = pos3 + change;
+          Serial.println(pos3);
+        } else {
+          Serial.println("This servo can't move past this angle");
         }
-      } else if(servo == '4'){
-        if(pos2 + change <= 180){
+      } else if (servo == '4') {
+        if (pos4 + change <= 180) {
           pos4 = pos4 + change;
+          Serial.println(pos4);
+        } else {
+          Serial.println("This servo can't move past this angle");
         }
       }
     }
